454cc798f87b2a241dc631c0ac93952632781126
Salohiddin Yoqubov <bcloudintelekt@gmail.com>
2025-01-11 18:02:37 +0500

    translated getting_started page fix 2

--- doc/en/conf.py
+++ doc/en/conf.py
@@ -167,7 +167,7 @@ html_favicon = "img/favicon.png"
 html_use_index = False
 html_show_sourcelink = False
 
-html_baseurl = "https://docs.pytest.org/en/stable/"
+html_baseurl = "https://pytest-uz.readthedocs.io/uz/latest/"
 
 # -- Options for HTML Help output -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-help-output
@@ -179,16 +179,16 @@ htmlhelp_basename = "pytestdoc"
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-manual-page-output
 
 man_pages = [
-    ("how-to/usage", "pytest", "pytest usage", ["holger krekel at merlinux eu"], 1)
+    ("how-to/usage", "pytest", "pytest usage", ["holger krekel at merlinux eu. O'zbek tilida Salohiddin Yoqubov"], 1)
 ]
 
 # -- Options for epub output ----------------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-epub-output
 
 epub_title = "pytest"
-epub_author = "holger krekel at merlinux eu"
-epub_publisher = "holger krekel at merlinux eu"
-epub_copyright = "2013, holger krekel et alii"
+epub_author = "holger krekel at merlinux eu. O'zbek tilida Salohiddin Yoqubov"
+epub_publisher = "holger krekel at merlinux eu. O'zbek tilida Salohiddin Yoqubov"
+epub_copyright = "2025, holger krekel et alii. O'zbek tilida Salohiddin Yoqubov"
 
 # -- Options for LaTeX output --------------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-latex-output
@@ -197,8 +197,8 @@ latex_documents = [
     (
         "contents",
         "pytest.tex",
-        "pytest Documentation",
-        "holger krekel, trainer and consultant, https://merlinux.eu/",
+        "pytest Qo'llanma",
+        "holger krekel, trainer and consultant O'zbek tilida Salohiddin Yoqubov, https://merlinux.eu/",
         "manual",
     )
 ]
@@ -228,7 +228,7 @@ texinfo_documents = [
         "pytest",
         "pytest Documentation",
         (
-            "Holger Krekel@*Benjamin Peterson@*Ronny Pfannschmidt@*"
+            "Holger Krekel@*Benjamin Peterson@*Ronny Pfannschmidt@*Salohiddin Yoqubov@*"
             "Floris Bruynooghe@*others"
         ),
         "pytest",
